ff20c181211f95c636d64f19e7157691515ee57a
nicofedora <nicolassandoval140692@gmail.com>
2026-09-19 10:44:19 -0300

    Bump version to 0.4.0

--- src/presentation/pages/SettingsPage.vue
+++ src/presentation/pages/SettingsPage.vue
@@ -23,7 +23,7 @@ onMounted(async () => {
     try {
         appVersion.value = await getVersion();
     } catch {
-        appVersion.value = "0.3.0";
+        appVersion.value = "0.4.0";
     }
     if (canConfigurarRecargoNocturno()) {
         await nocturnoStore.fetchConfig();
